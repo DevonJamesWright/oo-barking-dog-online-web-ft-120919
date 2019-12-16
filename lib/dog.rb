82cc18d806614b32d45da2b753ec4bae7bcf24e5
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-16 14:01:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog 
+  def dog_name (dog_name)
+    @dog_name = dog_name
+  end
+  def dog_name
+    @dog_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+
+end 
+fido = Dog.new 
+fido.dog_name = "Fido"
